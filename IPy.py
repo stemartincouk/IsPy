adb1eb89a2ec27181d5926d6a49c261a7a6389e6
Stephen Martin <me@stephen-martin.co.uk>
2011-09-15 14:12:26 +0100

    remove paths for files

--- IPy.py
+++ IPy.py
@@ -18,7 +18,7 @@ socket.setdefaulttimeout(60)
 
 class Conf:
     def __init__(self):
-        self.fileLocation="/opt/IPy/IPy.conf"
+        self.fileLocation="IPy.conf"
         self.mailServer=""
         self.mailServerAuth=""
         self.mailUser=""
